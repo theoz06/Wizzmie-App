d1dc47795f1197e2dbca2568183e5a65e16d7c02
Teofilus <t.ziraluo@yahoo.com>
2024-11-25 04:05:41 +0700

    Add URR fetch API Status Transaction

--- server-app/src/main/java/com/wizzmie/server_app/Config/MidtransConfig.java
+++ server-app/src/main/java/com/wizzmie/server_app/Config/MidtransConfig.java
@@ -1,5 +1,7 @@
 package com.wizzmie.server_app.Config;
 
+import java.util.Base64;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +10,42 @@ import com.midtrans.Midtrans;
 @Configuration
 public class MidtransConfig {
     
+    private final String serverKey = "SB-Mid-server-i1Zl0vN5onOKbk0CJ-EYr3bm";
+    private static final String baseUrl = "https://api.sandbox.midtrans.com/v2/charge";
+    private final String statusUrl = "https://api.sandbox.midtrans.com/v2/"; 
+    private final boolean isProduction = false;
+
+    private final String merchantId = "G950547607";
+
+
     @Bean
     public void configureMidtrans() {
-        Midtrans.serverKey = "SB-Mid-server-i1Zl0vN5onOKbk0CJ-EYr3bm";
-        Midtrans.isProduction = false;
+        Midtrans.serverKey = serverKey;
+        Midtrans.isProduction = isProduction;
+    }
+
+    public String getServerKey(){
+        return serverKey;
+    }
+
+    public String getMerchantId(){
+        return merchantId;
+    }
+
+    public boolean isProduction(){
+        return isProduction;
+    }
+
+    public String getAuthorizationHeader(){
+        String auth = serverKey + ":";
+        return "Basic " + Base64.getEncoder().encodeToString(auth.getBytes());
+    }
+
+    public String getBaseUrl(){
+        return baseUrl;
+    }
+
+    public String getStatusUrl(){
+        return statusUrl;
     }
 }
