e93e9433773ef4b7295d51ce24b65b18aad85726
Teofilus <t.ziraluo@yahoo.com>
2024-11-26 11:10:49 +0700

    Implemnt security, basic auth

--- server-app/src/main/java/com/wizzmie/server_app/Config/SecurityConfig.java
+++ server-app/src/main/java/com/wizzmie/server_app/Config/SecurityConfig.java
@@ -4,20 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.wizzmie.server_app.Services.Implements.UserDetailsServiceImpl;
 
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+
     @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+    private UserDetailsServiceImpl userDetailsServiceImpl;
 
 
     @Bean
@@ -35,14 +41,7 @@ public class SecurityConfig {
                                 .antMatchers("/api/order/customer/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                    .loginPage("/login")
-                    .permitAll()
-                )
-                .logout(logout -> logout
-                    .logoutSuccessUrl("/login")
-                    .permitAll()
-                );
+               .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
@@ -51,5 +50,24 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider (){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsServiceImpl);
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProviderBean() {
+        return authenticationProvider();
+        
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
