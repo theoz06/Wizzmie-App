6b12fa08d1988c9478f647f3e2dc3e3fe59b6435
Teofilus <t.ziraluo@yahoo.com>
2024-11-26 11:08:56 +0700

    Add Custom UserDetailsService

--- server-app/src/main/java/com/wizzmie/server_app/Services/Implements/UserDetailsServiceImpl.java
+++ server-app/src/main/java/com/wizzmie/server_app/Services/Implements/UserDetailsServiceImpl.java
@@ -3,15 +3,18 @@ package com.wizzmie.server_app.Services.Implements;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.wizzmie.server_app.Entity.User;
 import com.wizzmie.server_app.Repository.UserRepository;
 
 
-
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -25,9 +28,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         
         Optional<User> user = userRepository.findByNik(nik);
 
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.get().getRole().getUsersRole().toUpperCase());
+
         return org.springframework.security.core.userdetails.User
             .withUsername(user.get().getNik())
-            .authorities(user.get().getRole().getUsersRole())
+            .password(user.get().getPassword())
+            .authorities(authority)
             .build();
     }
     
