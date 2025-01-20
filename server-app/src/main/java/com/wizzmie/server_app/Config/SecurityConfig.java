9fb850143f324e56a6997ddc7aa29ad309fda92d
Teofilus <t.ziraluo@yahoo.com>
2025-01-20 21:03:04 +0700

    Update API base URL to use HTTP and modify CORS configuration to allow all origins

--- server-app/src/main/java/com/wizzmie/server_app/Config/SecurityConfig.java
+++ server-app/src/main/java/com/wizzmie/server_app/Config/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://wizzmie-cibaduyut.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://wizzmie-cibaduyut.vercel.app", "*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
