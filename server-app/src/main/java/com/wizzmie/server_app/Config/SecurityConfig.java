1a3efa7ae23aae6bd2a15d3f516b642956c64f35
Teofilus <t.ziraluo@yahoo.com>
2025-01-21 02:42:19 +0700

    Update CORS configuration to replace IP address for allowed origins

--- server-app/src/main/java/com/wizzmie/server_app/Config/SecurityConfig.java
+++ server-app/src/main/java/com/wizzmie/server_app/Config/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.107.68.141:3000", "https://wizzmie-cibaduyut.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://54.252.141.173:3000", "https://wizzmie-cibaduyut.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
